957863ef648b5ab7d03294f73c1396ebc77b187c
Duducito15 <echavezv@utea.edu.pe>
2025-02-20 20:26:18 -0500

    Environment

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:app00_movie/config/constants/environment.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = "home - screen";
@@ -8,7 +10,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Placeholder(),
-    );
+        body: Center(
+      child: Text(Environment.theMovieDbKey),
+    ));
   }
 }
